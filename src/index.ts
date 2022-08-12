e5c56644ceee819e7f183d5c63e38a80cf8d4a8e
Roy Pouls <github@synga.nl>
2022-08-12 11:33:59 +0200

    Definend more exports

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,16 @@
+// Entity
 export * from "./Entity";
+
+// Bags
+export * from "./Bag/Bag";
+export * from "./Bag/CastsBag";
+export * from "./Bag/AttributeBag";
+export * from "./Bag/RelationBag";
+
+// Configuration
 export * from "./Configuration";
 export * from "./config";
-export * from "./Casts/DateTimeCast";
+
+// Casts
 export * from "./Casts/Cast"
+export * from "./Casts/DateTimeCast";
